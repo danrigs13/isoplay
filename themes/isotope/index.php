a3948ea06b302954fea6702d4fab4c26456b6476
Dani <drigsby@hotmail.com>
2015-11-10 13:39:41 -0800

    Add single and functions files

---
+++ themes/isotope/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title><?php is_front_page() ? bloginfo('title') : wp_title(''); ?></title>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="/js/isotope.pkgd.min.js"></script>
+
+
+  </head>
+  <body>
+
+  </body>
+  <footer>
+
+  </footer>
+</html>
